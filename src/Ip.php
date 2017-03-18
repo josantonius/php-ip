2b48671917e1e05bcdca41b7fd857ab7b0aef79e
Josantonius <info@josantonius.com>
2017-03-18 05:08:55 +0100

    The repository was updated to version 1.1.1

--- src/Ip.php
+++ src/Ip.php
@@ -2,15 +2,11 @@
 /**
  * PHP class to get user IP.
  * 
- * @category   JST
- * @package    Ip
- * @subpackage Ip
  * @author     Tobias Sette Class - http://github.com/gnumoksha 
- * @copyright  Copyright (c) 2017 JST PHP Framework
+ * @copyright  Copyright (c) 2017
  * @license    https://opensource.org/licenses/MIT - The MIT License (MIT)
- * @version    1.1.0
  * @link       https://github.com/Josantonius/PHP-Ip
- * @since      File available since 1.0.0 - Update: 2017-01-30
+ * @since      1.0.0
  */
 
 namespace Josantonius\Ip;
